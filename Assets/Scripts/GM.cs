14920e10c08b3adf855c91cb2c065fc574b54236
Landon <landonscribbles@gmail.com>
2017-04-23 19:40:34 -0700

    Upload of Project

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour {
+
+    public int lives = 3;
+    public int bricks = 20;
+    public float resetDelay = 1f;
+    public Text livesText;
+    public GameObject gameOver;
+    public GameObject youWon;
+    public GameObject bricksPrefab;
+    public GameObject paddle;
+    public GameObject deathParticles;
+    public static GM instance = null;
+
+    private GameObject clonePaddle;
+
+	void Start () {
+        Cursor.visible = false; 
+		if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+        Setup();
+	}
+
+    public void Setup() {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity);
+        Instantiate(bricksPrefab, transform.position, Quaternion.identity);
+    }
+
+    void CheckGameOver() {
+        if (bricks < 1) {
+            youWon.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke("Reset", resetDelay);
+        }
+
+        if (lives < 1) {
+            gameOver.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke("Reset", resetDelay);
+        }
+    }
+
+    private void Reset() {
+        Time.timeScale = 1f;
+        // TODO: Update to SceneManager
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void LoseLife() {
+        lives--;
+        livesText.text = "Lives: " + lives;
+        Instantiate(deathParticles, clonePaddle.transform.position, Quaternion.identity);
+        Destroy(clonePaddle);
+        Invoke("SetupPaddle", resetDelay);
+        CheckGameOver();
+    }
+
+    void SetupPaddle() {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity);
+    }
+
+    public void DestroyBrick() {
+        bricks--;
+        CheckGameOver();
+    }
+
+    private bool CheckForEsc() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void Update() {
+        if (CheckForEsc()) {
+            Application.Quit();
+            Cursor.visible = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    public float fallSpeed = 1;
+    private Renderer renderer;
+
+    private void Start() {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update () {
+        float timeFloatSpeed = fallSpeed * Time.deltaTime;
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - timeFloatSpeed, this.transform.position.z);
+	}
+
+    void DestroyIfNotVisible() {
+        //FIXME
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if (other.gameObject.tag == "paddle") {
+            Debug.Log("Grabbed a power up!");
+            Destroy(gameObject);
+        }
+    }
+}
